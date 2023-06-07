8ffc45bb2a4f6c04c98bf806ee6e89808054936f
oozoemenam <oozoemenam@gmail.com>
2023-06-07 22:03:48 +0200

    first commit

---
+++ src/test/java/com/springngwebsocket/SpringNgWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springngwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNgWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
