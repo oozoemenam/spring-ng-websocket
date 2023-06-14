acfdae6290c8110496fae0735e9d39f001c9c2c0
David Ozoemenam <oozoemenam@gmail.com>
2023-06-14 12:57:28 +0200

    add websocket client

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import $ from 'jquery';
+import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'spring-ng-websocket';
+  public title = 'WebSocket';
+  private serverUrl = 'http://localhost:8080/socket';
+  private stompClient: any;
+
+  constructor() {
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection() {
+    const ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    const that = this;
+    this.stompClient.connect({}, (frame: any) => {
+      that.stompClient.subscribe('/chat', (message: any) => {
+        if (message.body) {
+          $('.chat').append('<div class="message">' + message.body + '</div>');
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message: any) {
+    this.stompClient.send('/app/send/message', {}, message);
+    $('#input').val('');
+    // document.getElementById('input').
+  }
 }
